fddb5a0cf25737f2a358aed1dc3d6ea28e2e391d
SATOTAKUYA <>
2018-12-02 02:37:53 +0900

    ac

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with :name => takuya, :password => takuya4645 if Rails.env == "production"
   before_action :configure_account_update_params, only:[:profile_update]
   before_action :configure_permitted_parameters,if: :devise_controller?
 

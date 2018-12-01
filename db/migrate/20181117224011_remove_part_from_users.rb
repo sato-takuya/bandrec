717d064c638094ce9e324d2679dc13c3e69ef773
SATOTAKUYA <>
2018-12-02 02:07:31 +0900

    リムーブカラム

--- db/migrate/20181117224011_remove_part_from_users.rb
+++ db/migrate/20181117224011_remove_part_from_users.rb
@@ -1,5 +1,5 @@
 class RemovePartFromUsers < ActiveRecord::Migration[5.2]
   def change
-    remove_column :users,:part,:integer
+    #remove_column :users,:part,:integer
   end
 end
--- db/migrate/20181117224115_remove_instrumentpart_from_users.rb
+++ db/migrate/20181117224115_remove_instrumentpart_from_users.rb
@@ -1,5 +1,5 @@
 class RemoveInstrumentpartFromUsers < ActiveRecord::Migration[5.2]
   def change
-    remove_column :users,:instrument_part,:text
+    #remove_column :users,:instrument_part,:text
   end
 end

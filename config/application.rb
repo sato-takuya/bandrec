20bc2c5e1969ea61ad6d40d7c049ca033622c868
SATOTAKUYA <>
2018-12-07 22:52:59 +0900

    no message

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,6 @@ module Bandrec
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-    config.assets.initialize_on_precompile = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers

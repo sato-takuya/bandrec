212870a46640c3c82b60c2281083d01dad81a04a
SATOTAKUYA <>
2018-12-07 23:18:02 +0900

    no message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.action_mailer.default_url_options = { :host => 'https://bandrec.herokuapp.com' }
-  config.action_mailer.raise_delivery_errors = false　#この一文も追記!!
+  config.action_mailer.raise_delivery_errors = true　#この一文も追記!!
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :user_name => "app117780894@heroku.com",

4d095f54d30362ba10256f10b95e6b1d30daa6c7
SATOTAKUYA <>
2018-12-09 16:53:30 +0900

    no message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
 
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:area])
   end
 
 end

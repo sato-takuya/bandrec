23abe903fa0fdb24d23d8cd044e016dde93a9844
SATOTAKUYA <>
2018-12-10 21:21:47 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:area])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
 end

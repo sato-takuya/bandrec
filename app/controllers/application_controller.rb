b1bee347985bf3ea25e2cc1f5998e7404ee25652
SATOTAKUYA <>
2018-12-13 22:57:09 +0900

    no message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,19 +5,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
-  #herokuapp.comから独自ドメインへリダイレクト
-  before_filter :ensure_domain
-  FQDN = 'www.anzujam.net'
-
-  # redirect correct server from herokuapp domain for SEO
-  def ensure_domain
-   return unless /\.bandrec.net/ =~ request.host
-
-   # 主にlocalテスト用の対策80と443以外でアクセスされた場合ポート番号をURLに含める
-   port = ":#{request.port}" unless [80, 443].include?(request.port)
-   redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently
-  end
-
 
 
 

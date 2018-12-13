36de7b62d6fa970ddfd2517965146a8728d6aadf
SATOTAKUYA <>
2018-12-14 01:23:17 +0900

    no message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
 
-  config.action_mailer.default_url_options = { host: 'https://www.bandrec.net' }
+  config.action_mailer.default_url_options = { host: 'https://www.anzujam.net' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {

2e59c75ad65a9aef3305a29b11574a477d515034
SATOTAKUYA <>
2018-12-15 20:56:40 +0900

    no message

--- app/mailers/arrival_mailer.rb
+++ app/mailers/arrival_mailer.rb
@@ -2,6 +2,6 @@ class ArrivalMailer < ApplicationMailer
   def send_when_create(user)
     @user = user #メールの文面用のインスタンス変数
     mail to:      user.email,
-          subject: '【anzujam】新着メッセージがあります'
+          subject: '【ands jam】新着メッセージがあります'
   end
 end
